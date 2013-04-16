45ab60a06d3c3ecf13735a879d17203cf2a9d1a1
David Mills <dave@1morepx.com>
2013-04-16 15:10:30 -0400

    inital commit
    - removed public/index.html
    - added basic gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaHours::Application.config.secret_token = 'f3daf44cf1d775be6e463d2bedf1e66c90542c4765d2f8399d77cb2c0503726ccde4235b120d83d91e2eb931426c313efebb13a6f782248d7fbeda7034d01653'
